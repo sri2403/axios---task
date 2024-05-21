40285b446f58753f4290823a30cb8a5b5b8cb1fc
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-05-21 22:18:25 +0530

    updated

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
+        <div>
         <nav className="navbar">
             <ul>
                 <li><Link to="/">Home</Link></li>
@@ -11,6 +12,10 @@ const Nav = () => {
                 <li><Link to="/create">Create</Link></li>
             </ul>
         </nav>
+        <div className='text-center p-2'>
+        <p style={{ fontStyle: 'italic', color: 'black' }}>If the updates aren't showing, a quick refresh should bring them into view.Thank you..!!</p>
+    </div>
+    </div>
     );
 };
 
